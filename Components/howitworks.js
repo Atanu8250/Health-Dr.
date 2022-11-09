29dff2c4e8e7c8ad62752d1ad98dbda462b39479
ThilakSamplegithub <112916543+ThilakSamplegithub@users.noreply.github.com>
2022-11-09 23:47:58 +0530

    day2

---
+++ Components/howitworks.js
@@ -0,0 +1,3 @@
+//https://i0.wp.com/loseitblog.com/wp-content/uploads/2018/09/Why-Calories-Matter.png?w=1010&ssl=1
+//https://i0.wp.com/loseitblog.com/wp-content/uploads/2019/10/Untitled-design-143.png?w=1010&ssl=1
+// https://i0.wp.com/loseitblog.com/wp-content/uploads/2020/03/Untitled-design-207.png?w=1010&ssl=1
