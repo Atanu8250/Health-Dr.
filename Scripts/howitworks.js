536b42f31960e7198110ed1398421a383677c09b
Atanu8250 <atanu20uparsole@gmail.com>
2022-11-13 15:41:48 +0530

    page binding

--- Scripts/howitworks.js
+++ Scripts/howitworks.js
@@ -2,6 +2,11 @@ import footer from "../Components/footer.js";
 
 document.querySelector('#footer').innerHTML = footer();
 
+
+document.querySelector('#topthLogo').onclick = () => {
+    window.location.href = 'index.html';
+}
+
 //https://i0.wp.com/loseitblog.com/wp-content/uploads/2018/09/Why-Calories-Matter.png?w=1010&ssl=1
 //https://i0.wp.com/loseitblog.com/wp-content/uploads/2019/10/Untitled-design-143.png?w=1010&ssl=1
 // https://i0.wp.com/loseitblog.com/wp-content/uploads/2020/03/Untitled-design-207.png?w=1010&ssl=1
